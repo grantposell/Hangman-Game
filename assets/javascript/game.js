1efb5f516c50a05324ff30b5f93ba25fb2685ac8
Grant Posell <grantposell@Grants-MacBook-Pro.local>
2018-02-14 00:13:32 -0600

    JavaScript variables

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,17 +1,74 @@
+//Global Variables
+//================================================================
+//Arrays and variables for holding data
 var randomWordFood = ["ceviche","crepes", "dumplings", "fajitas" , "gnocchi" , "pasta" , "pizza" , "ramen" , "schnitzel" , "tacos"];
-var randomWord = randomWordFood[Math.floor(Math.random() * randomWordFood.length)];
+var selectedWord = "";
+var lettersInWord = [];
+var numberBlanks = 0;
+var blanksAndSuccesses = [];
+var wrongLetters =[];
 
-var f;
-var count = 0;
-var answerArray = [];
+//Game Counters
+var winCount = 0;
+var lossCount = 0;
+var guessesLeft = 0;
 
-function startUp()
-{
-    for (var i = 0; i < randomWord.length; i++)
+//Functions
+function startGame () {
+    selectedWord = randomWordFood[Math.floor(Math.random() * randomWordFood.length)];
+    lettersInWord = selectedWord.split("");
+    numberBlanks = lettersInWord.length;
+
+    //Reset
+    guessesLeft = 9;
+    wrongLetters = [];
+    blanksAndSuccesses = [];
+    
+    //Putting words in the blanks
+    for (var i = 0; i < numberBlanks; i++)
     {
-        answerArray[i] = "_";
+        blanksAndSuccesses.push("_");
     }
 
-    f = answerArray.join(" ");
-    document.getElementById("answer").innerHTML = f;
+    //Changing HTML
+    document.getElementById("wordToGuess").innerHTML = blanksAndSuccesses.join(" ");
+    document.getElementById("numGuesses").innerHTML = guessesLeft;
+    document.getElementById("winCounter").innerHTML = winCount;
+    document.getElementById("lossCounter").innerHTML = lossCount;
+    //Testing
+    console.log(selectedWord);
+    console.log(lettersInWord);
+    console.log(numberBlanks);
+    console.log(blanksAndSuccesses);
+}
+function checkLetters(letter) {
+    // Checks if letter exists in code
+    var isLetterInWord = false;
+    for (var i=0; i<numberBlanks; i++){
+        if(selectedWord[i] == letter){
+            isLetterInWord = true;
+        }
+    }
+    //Check where in the word the letter it exists
+    if (isLetterInWord){
+        for (var i=0; i<numberBlanks; i++){
+            if(selectedWord[i] == letter){
+                blanksAndSuccesses[i] = letter;
+            }
+        }
+    }
+    else{
+        wrongLetters.push(letter);
+        numGuesses--
+    }
+}
+//Main Process
+//Starts the code for the first time
+startGame();
+
+//Registering keycode
+
+document.onkeyup = function(event) {
+    var letterGuessed = String.fromCharCode(event.keyCode).toLowerCas();
+    console.log(letterGuessed);
 }
